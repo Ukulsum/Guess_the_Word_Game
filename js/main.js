42ebf45646a2ab2d11ee7bda5688b4afa73bbd8e
Ukulsum <ukulsum1280@gmail.com>
2022-09-26 23:49:20 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,433 @@
+const wordList = [
+
+    {
+        word : "python",
+        hint : "Programming language"
+    },
+    {
+        word : "guitar",
+        hint : "A musical instrument"
+    },
+    {
+        word : "aim",
+        hint : "A purpose or intention"
+    },
+    {
+        word : "venus",
+        hint : "Planet of our solar system"
+    },
+    {
+        word : "gold",
+        hint : "A yellow precious metal"
+    },
+    {
+        word : "air",
+        hint : "The invisible gaseous"
+    },
+    {
+        word : "idea",
+        hint : "A thought or suggestion"
+    },
+    {
+        word : "server",
+        hint : "Related to computer or system"
+    },
+    {
+        word : "svg",
+        hint : "A vector image format"
+    },
+
+    {
+        word : "jpeg",
+        hint : "A simple image file format"
+    },
+
+    {
+        word : "search",
+        hint : "Act to find something"
+    },
+    {
+        word : "golang",
+        hint : "Programming language"
+    },
+    {
+        word : "crypto",
+        hint : "Related to cryptocurrency"
+    },
+    {
+        word : "tesla",
+        hint : "Unit of magnetic flux density"
+    },
+    {
+        word : "rocket",
+        hint : "Space vehicle or missile"
+    },
+    {
+        word : "mars",
+        hint : "Second smallest planet"
+    },
+    {
+        word : "github",
+        hint : "Code hosting platform"
+    },
+    {
+        word : "png",
+        hint : "Type of raster image file"
+    },
+    {
+        word : "silver",
+        hint : "Precious  greyish-white metal"
+    },
+    {
+        word : "dhaka",
+        hint : "It's a city found in Bangladesh"
+    },
+    {
+        word : "google",
+        hint : "Famous search engine"
+    },
+    {
+        word : "apple",
+        hint : "Mobile manufacture brand"
+    },
+    {
+        word : "proxy",
+        hint : "Related to server application"
+    },
+    {
+        word : "email",
+        hint : "Related to exchanging message"
+    },
+    {
+        word : "banana",
+        hint : "Yellow long curved fruit"
+    },
+    {
+        word : "act",
+        hint : "The doing of the thing"
+    },
+    {
+        word : "chess",
+        hint : "Game played by two players"
+    },
+    {
+        word : "matrix",
+        hint : "Science fiction movie"
+    },
+    {
+        word : "bugs",
+        hint : "Related to programming"
+    },
+    {
+        word : "gif",
+        hint : "A file format for image"
+    },
+    {
+        word : "joking",
+        hint : "Humorous or flippant"
+    },
+    {
+        word : "mental",
+        hint : "Relating to the mind"
+    },
+    {
+        word : "avatar",
+        hint : "Epic science fiction film"
+    },
+
+    {
+        word : "map",
+        hint : "Diagram representation of area"
+    },
+    {
+        word : "island",
+        hint : "Land surrounded by water"
+    },
+    {
+        word : "hockey",
+        hint : "A famous outdoor game"
+    },
+    {
+        word : "coding",
+        hint : "Related to programming"
+    },
+    {
+        word : "venice",
+        hint : "Famous city of water"
+    }, 
+    {
+        word : "mysql",
+        hint : "A relational database system"
+    }, 
+    {
+        word : "nepal",
+        hint : "A developing country"
+    },
+    {
+        word : "computer",
+        hint : "Electronic brain"
+    },
+    {
+        word : "facebook",
+        hint : "Social Media Network"
+    },
+    {
+        word : "currency",
+        hint : "System of money in general use"
+    },
+    {
+        word : "tiger",
+        hint : "It's a ferocious animal"
+    },
+    {
+        word : "decency",
+        hint : "Proper Behaviour"
+    },
+    {
+        word : "approval",
+        hint : "Favourable opinion"
+    },
+    {
+        word : "criticism",
+        hint : "Fault finding"
+    },
+    {
+        word : "gift",
+        hint : "A present"
+    },
+    {
+        word : "friday",
+        hint : "It's a Holiday"
+    },
+    {
+        word : "marriage",
+        hint : "Fixed in heaven"
+    },
+    {
+        word : "photograph",
+        hint : "Holds your memory"
+    },
+    {
+        word : "nepalgir",
+        hint : "Beijing is it's capital"
+    },
+    {
+        word : "june",
+        hint : "Middle month"
+    },
+    {
+        word : "joker",
+        hint : "Makes you laugh"
+    },
+    {
+        word : "aquarium",
+        hint : "Pool for fishes"
+    },
+    {
+        word : "pillow",
+        hint : "Support the head when lying"
+    },
+    {
+        word : "garden",
+        hint : "Area for growing plants"
+    },
+    {
+        word : "mask",
+        hint : "Protect you from covid-19"
+    },
+    {
+        word : "mirror",
+        hint : "I will do what you do"
+    },
+    {
+        word : "january",
+        hint : "It's a first month"
+    },
+    {
+        word : "music",
+        hint : "Art of producing tones"
+    },
+    {
+        word : "cycling",
+        hint : "Involves lot of pedaling"
+    },
+    {
+        word : "movie",
+        hint : "Another word for film"
+    },
+    {
+        word : "beijing",
+        hint : "It's a city found in China"
+    },
+    {
+        word : "rugby",
+        hint : "It's a sport played with oval ball"
+    },
+    {
+        word : "iceland",
+        hint : "Land of fire and ice"
+    },
+    {
+        word : "china",
+        hint : "Beijing is it's capital"
+    },
+    {
+        word : "kinetic",
+        hint : "It's a kind of energy"
+    },
+    {
+        word : "hyena",
+        hint : "It's an African animal"
+    },
+    {
+        word : "gravitry",
+        hint : "Force that causes things to drop to Earth"
+    },
+    {
+        word : "giraffe",
+        hint : "It has long neck and long leg"
+    },
+    {
+        word : "sputnik",
+        hint : "First artificial satellite orbiting around earth"
+    },
+    {
+        word : "elephant",
+        hint : "Bigest animal"
+    },
+    {
+        word : "pelican",
+        hint : "Bird carries fish in it's sack-like bill"
+    },
+    {
+        word : "library",
+        hint : "Place having lot of books"
+    },
+    {
+        word : "tadpole",
+        hint : "Larval stage of a frog"
+    },
+    {
+        word : "milk",
+        hint : "It's a white liquid"
+    },
+    {
+        word : "rabbit",
+        hint : "It's a long-eared animal"
+    },
+    {
+        word : "amazon",
+        hint : "It's a widest river"
+    },
+    {
+        word : "abacus",
+        hint : "IT's a counting instrument"
+    },
+    {
+        word : "summer",
+        hint : "It's a hot season"
+    },
+    {
+        word : "poland",
+        hint : "It's a European country"
+    },
+    {
+        word : "candle",
+        hint : "It's made of wax"
+    },
+    {
+        word : "oxygen",
+        hint : "It's a gas"
+    },
+    {
+        word : "hTML",
+        hint : "It's a markup language"
+    },
+
+]
+
+
+let inputs = document.querySelector(".inputs");
+let reset_btn = document.querySelector(".reset_btn");
+let hints = document.querySelector(".hint span");
+let typing_input = document.querySelector(".typing_input");
+let wrong_letter = document.querySelector(".wrong_letter span");
+let guesses = document.querySelector(".guesses span")
+
+
+let word, maxGuesses, corrects = [], incorrects = [];
+
+function randomWord(){
+//getting random object form wordlist
+    let random_object = wordList[Math.floor(Math.random() * wordList.length)];
+  //getting word of random object
+     word = random_object.word;
+     maxGuesses = 8; corrects = []; incorrects = [];
+
+     hints.innerHTML = random_object.hint;
+     guesses.innerHTML = maxGuesses;
+     wrong_letter.innerHTML = incorrects;
+
+ 
+  //create inputs tag according to the word length
+    let html = "";
+    for(let i = 0; i < word.length; i++){
+        html += `<input type="text" disabled>`;
+    }
+    inputs.innerHTML = html; 
+
+  //show the hint of the word
+    hints.innerHTML = random_object.hint;
+}
+
+
+//calling random object from the wordlist
+randomWord();
+
+function initGame(e){
+    let key = e.target.value;
+ //validate only user pressed key is alphabet character
+      if(key.match(/^[A-Za-z]+$/) && !incorrects.includes(` ${key} `) && !corrects.includes(key)){
+       // if  user letter founded in the word
+           if(word.includes(key)){
+         //show the found letter in input
+             for(let i = 0; i < word.length; i++){
+          // showing match letter in the input value
+              if(word[i] === key){
+                corrects.push(key);
+                inputs.querySelectorAll("input")[i].value = key;
+               }
+           } 
+      }else{
+        //decrement maxGuesses by 1
+        maxGuesses--;
+        incorrects.push(` ${key} `);
+     }
+     guesses.innerHTML = maxGuesses;
+     wrong_letter.innerHTML = incorrects;
+    }
+  //empty the input tag once user entered any key
+    typing_input.value = "";
+
+    setTimeout(() => {
+        //if user found all letters
+    if(corrects.length === word.length){
+        alert(`Congrats! You found the word ${word.toUpperCase()}`);
+       //calling randomword func, so the game reset
+       randomWord();
+    }
+  //if user couldn't found all letters
+    if(maxGuesses < 1){
+          alert("Game is over! You don't have remaining guesses");
+          for(let i = 0; i < word.length; i++){
+             // show all letter in the input 
+                inputs.querySelectorAll("input")[i].value = word[i];
+              } 
+     }
+    })
+}
+//get the random word on the button click
+reset_btn.addEventListener("click", randomWord);
+typing_input.addEventListener("input", initGame);
+//Automatically focusing input when user pressed any key
+document.addEventListener("keydown", () => typing_input.focus());
